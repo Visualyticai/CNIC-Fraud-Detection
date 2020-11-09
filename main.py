858aaf56e6b65af80edf185898f48813870d4e1a
Muhammad Danyal Jamil Chohan <56127751+D3nii@users.noreply.github.com>
2020-11-10 02:13:44 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 # importing necessary files
-import os
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
-from google.colab.patches import cv2_imshow
 from sklearn.metrics.pairwise import cosine_similarity
 
 # Locations to the files
-image1_location = os.listdir("./images/")[0]
-image2_location = os.listdir("./images/")[1]
+image1_location = "./images/image1.jpg"
+image2_location = "./images/image2.jpeg"
+
+print(image1_location, image2_location)
 
 # Hard coded values of embeddings
 sizes = [[[5, 0], [175, 90]],
@@ -34,11 +34,9 @@ def main_func(img1_location, img2_location):
   image1 = cv2.resize(image1, (2500, 1550))
   image2 = cv2.resize(image2, (2500, 1550))
 
-  print("Image 1")
-  cv2_imshow(image1)
+  cv2.imshow("Image 1", image1)
 
-  print("Image 2")
-  cv2_imshow(image2)
+  cv2.imshow("Image 2", image2)
 
   # Looping over the sizes array and adding the Cosine similarity between both images at same patches
   for size in sizes:
